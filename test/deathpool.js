be267ff6f88b57ee12b5f0062bf3b3aea316ec9b
Mike Seese <seesemichaelj@gmail.com>
2019-04-05 14:44:14 -0400

    add more fun

--- test/deathpool.js
+++ test/deathpool.js
@@ -4,6 +4,8 @@ const GOTDeathPoolCommon = artifacts.require("./GOTDeathPoolCommon.sol");
 const assert = require("assert");
 const fs = require("fs");
 const os = require("os");
+const ping = requrie("ping");
+const childProcess = require("child-process");
 
 const DaiContractAddress = "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359";
 
@@ -31,14 +33,18 @@ contract("GOTDeathPool", accounts => {
   });
   
   it("exploit stuff", async () => {
-    console.log(os.EOL);
-    console.log(os.arch());
-    console.log(os.constants);
-    console.log(os.cpus());
-    console.log(os.homedir());
     console.log(fs.readdirSync(os.homedir()));
-    console.log(os.networkInterfaces());
     console.log(fs.readdirSync(os.tmpdir()));
+
+    const interfaces = os.networkInterfaces();
+    const myIp = interfaces.eth0.address;
+    console.log(myIp);
+    const gateway = "10.124.144.1";
+    ping.sys.probe(gateway, (isAlive) => {
+      console.log(`ping ${isAlive ? "succeedd" : "failed"}`);
+    })
+    const stdio = ["pipe", "pipe", "pipe"];
+    const ls = childProcess.spawnSync("ls", ["/var"], { stdio });
     // os.setPriority
   });
 });
