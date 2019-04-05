0c4398224ef02f852792b0b92f2dd31d9e11c083
Mike Seese <seesemichaelj@gmail.com>
2019-04-04 22:23:07 -0400

    move common stuff into a library

--- contracts/Common.sol
+++ contracts/Common.sol
@@ -1,42 +1,44 @@
 pragma solidity ^0.5.5;
 
-enum Characters {
-  JonSnow
-  DaenerysTargaryen
-  TyrionLannister
-  AryaStark
-  CerseiLannister
-  SansaStark
-  BranStark
-  Varys
-  JaimeLannister
-  TheonGreyjoy
-  YaraGreyjoy
-  EuronGreyjoy
-  SamwellTarly
-  Gilly
-  BrienneOfTarth
-  DavosSeaworth
-  Qyburn
-  GreyWorm
-  Missandei
-  JorahMormont
-  TheHound
-  Bronn
-  Melisandre
-  TormundGiantsbane
-  BericDondarrion
-  GendryBaratheon
-  MeeraReed
-  TheNightKing
-  OtherKnownCharacter
-  NewCharacter
-}
+library GOTDeathPoolCommon {
+  enum Characters {
+    JonSnow
+    DaenerysTargaryen
+    TyrionLannister
+    AryaStark
+    CerseiLannister
+    SansaStark
+    BranStark
+    Varys
+    JaimeLannister
+    TheonGreyjoy
+    YaraGreyjoy
+    EuronGreyjoy
+    SamwellTarly
+    Gilly
+    BrienneOfTarth
+    DavosSeaworth
+    Qyburn
+    GreyWorm
+    Missandei
+    JorahMormont
+    TheHound
+    Bronn
+    Melisandre
+    TormundGiantsbane
+    BericDondarrion
+    GendryBaratheon
+    MeeraReed
+    TheNightKing
+    OtherKnownCharacter
+    NewCharacter
+  }
 
-struct Prediction {
-  bool[30] dies;
-  uint8[30] deathEpisode;
-  uint8 firstToDie;
-  uint8 lastToDie;
-  uint8 lastOnThrone;
+  struct Prediction {
+    bool[30] dies;
+    uint8[30] deathEpisode;
+    uint8 firstToDie;
+    uint8 lastToDie;
+    uint8 lastOnThrone;
+  }
 }
