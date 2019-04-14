47869ca4887c6bf23fdd997a3913764268bd5ce4
Mike Seese <seesemichaelj@gmail.com>
2019-04-14 04:08:38 -0400

    add truth contract creation

---
+++ docs/create-truth-script.js
@@ -0,0 +1,41 @@
+let artifact;
+let contract;
+
+window.addEventListener('load', function() {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    web3js = new Web3(web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  web3js.eth.defaultAccount = web3js.eth.accounts[0];
+  erc20 = web3js.eth.contract(erc20abi);
+
+  $.getJSON("GOTDeathPoolTruth.json", (data) => {
+    artifact = data;
+
+    contract = web3js.eth.contract(artifact.abi);
+  
+    // Now you can start your app & access web3 freely:
+    haveWeb3 = true;
+  });
+})
+
+function deploy() {
+  if (haveWeb3) {
+    contract.new({
+      data: artifact.bytecode
+    }, (err, result) => {
+      if (err) {
+        //
+      }
+      else {
+        //
+      }
+    });
+  }
+}
